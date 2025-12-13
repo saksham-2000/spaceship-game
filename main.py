807c8189e78d3b222da458c65766eab560373314
Saksham Garg <sakshamgarg@MacBook-Pro-4.local>
2025-12-12 23:00:35 -0600

    added metoer shower, laser shooting with cooldown, collision handling, lifelines,

--- main.py
+++ main.py
@@ -3,9 +3,31 @@ import os
 import random
 import time
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1280,720
-MARGIN = 20
-FPS = 60
+from settings import *
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # returns pygame.Surface
+clock = pygame.time.Clock()
+pygame.display.set_caption('Space Shooter')
+runningLives = 5
+
+# creating sprite groups
+all_sprites_group = pygame.sprite.Group()
+meteor_sprites = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
+
+
+# importing surfaces
+star_surf = pygame.image.load(os.path.join('images', 'star.png')).convert_alpha()
+laser_surf = pygame.image.load(os.path.join('images', 'laser.png')).convert_alpha()
+meteor_surf = pygame.image.load(os.path.join('images', 'meteor.png')).convert_alpha()
+
+
+METEOR_EVENT = pygame.USEREVENT + 1
+# Set the timer to trigger every 2000 milliseconds (2 seconds)
+pygame.time.set_timer(METEOR_EVENT, 1000)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups, filename, starting_position = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)):
@@ -13,7 +35,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(os.path.join('images',filename)).convert_alpha()
         self.rect = self.image.get_frect(center = starting_position)
         self.direction = pygame.math.Vector2()
-        self.speed = 200
+        self.speed = 500
+
+        # firing attributes
+        self.can_shoot = True
+        self.last_laser_shoot = 0
+        self.cooldown = 400  # milliseconds
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -22,9 +49,17 @@ class Player(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction 
         self.rect.center += self.direction * self.speed * dt
 
+        current_time = pygame.time.get_ticks()
+       # print(current_time)
+        if not self.can_shoot and self.last_laser_shoot + self.cooldown <= current_time:
+            self.can_shoot = True
+
         recent_keys = pygame.key.get_just_pressed()
-        if recent_keys[pygame.K_SPACE]:
-            print('fire laser')
+        if recent_keys[pygame.K_SPACE] and self.can_shoot:
+            Laser((all_sprites_group, laser_sprites),laser_surf,pos = self.rect.midtop)
+            self.last_laser_shoot = current_time
+            self.can_shoot = False
+
 
 
 class Star(pygame.sprite.Sprite):
@@ -33,31 +68,67 @@ class Star(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_frect(center = (random.randint(0, SCREEN_WIDTH),random.randint(0,SCREEN_HEIGHT)))
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # returns pygame.Surface
-clock = pygame.time.Clock()
-pygame.display.set_caption('Space Shooter')
-running = True
+
+class Laser(pygame.sprite.Sprite):
+     def __init__(self, groups, surf, pos):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(midbottom = pos)
+        self.speed = 500
+
+     def update(self,dt):
+        self.rect.centery -= self.speed * dt
+        if self.rect.centery < 0:
+            self.kill() # ungroup
 
 
-# creating a group
-all_sprites_group = pygame.sprite.Group()
+class Meteor(pygame.sprite.Sprite):
+     def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (random.randint(0,SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT/10)))
+        self.direction = pygame.Vector2(random.uniform(-0.5, 0.5),1)
+        self.speed = 250
 
-# star surface
-star_surf = pygame.image.load(os.path.join('images', 'star.png')).convert_alpha()
-for i in range(20):
+     def update(self,dt):
+        self.rect.center += self.speed * self.direction * dt
+        if self.rect.top > SCREEN_HEIGHT:
+            self.kill() # ungroup
+        
+class LifeLine(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.font = pygame.font.Font(None, 40) 
+        global runningLives
+        self.image = self.font.render(f"Lives: {runningLives}", True, "white")
+        self.rect = self.image.get_frect(center = (60,30))
+
+    def update(self, dt):
+        self.image = self.font.render(f"Lives: {runningLives}", True, "white" if runningLives > 2 else "red")
+
+
+def handle_collisions():
+    global runningLives
+
+    player_collision = pygame.sprite.spritecollide(spaceship, meteor_sprites, dokill = True)
+    if player_collision:
+        runningLives -= 1
+    
+    for laser in laser_sprites:
+        laser_collisions = pygame.sprite.spritecollide(laser, meteor_sprites, dokill = True)
+        if len(laser_collisions):
+            laser.kill()
+
+
+for _ in range(20):
     Star(all_sprites_group, star_surf)
-
 
 spaceship = Player(all_sprites_group, filename='player.png', starting_position=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - MARGIN))
 
+lifeline = LifeLine(all_sprites_group)
 
-# meteor surface
-meteor = pygame.image.load(os.path.join('images','meteor.png')).convert_alpha() # loading a surface
-meteor_rect = meteor.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
-while running:
+while runningLives > 0:
 
     dt = clock.tick(FPS)/1000  # set frame rate
 
@@ -65,13 +136,19 @@ while running:
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            runningLives = False
+        elif event.type == METEOR_EVENT:
+            Meteor((all_sprites_group, meteor_sprites), meteor_surf)
+
+    handle_collisions()
+
 
     all_sprites_group.update(dt)
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("darkgray")
+    screen.fill("darkslateblue")
+
     
-    screen.blit(meteor,meteor_rect)
+    #screen.blit(meteor,meteor_rect)
 
     all_sprites_group.draw(screen)
 
